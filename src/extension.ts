614815470ab1cda0dddf1d5aaeb03fa69f5111ff
Robin Bentley <robinbentley@me.com>
2016-08-31 13:00:21 +0100

    Autocomplete for CSS properties

--- src/extension.ts
+++ src/extension.ts
@@ -3,13 +3,20 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+import SassCompletion from './sassAutocomplete';
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  vscode.languages.setLanguageConfiguration('sass-indented', {
+    wordPattern: /(#?-?\d*\.\d\w*%?)|([$@#!.:]?[\w-?]+%?)|[$@#!.]/g
+  });
 
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "sass-indented" is now active!');
+  const sassCompletion = new SassCompletion();
+  const sassCompletionRegister =
+    vscode.languages.registerCompletionItemProvider('sass-indented', sassCompletion, '\\.', '-', '$', '&', '@');
+
+  context.subscriptions.push(sassCompletionRegister);
 }
 
 // this method is called when your extension is deactivated
