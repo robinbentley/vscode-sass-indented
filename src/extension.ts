3b39a4c95409cf6c549513c91bb45ff6c25964ab
Robin Bentley <robinbentley@me.com>
2016-09-02 16:47:38 +0100

    Stop autocomplete triggering on '&'

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
 import SassCompletion from './sassAutocomplete';
 
 // this method is called when your extension is activated
@@ -14,7 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
 
   const sassCompletion = new SassCompletion();
   const sassCompletionRegister =
-    vscode.languages.registerCompletionItemProvider('sass-indented', sassCompletion, '\\.', '-', '$', '&', '@');
+    vscode.languages.registerCompletionItemProvider('sass-indented', sassCompletion, '\\.', '-', '$', '@');
 
   context.subscriptions.push(sassCompletionRegister);
 }
